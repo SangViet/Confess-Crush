d56a6d834910fb48a0ca365bca47d9789bdef9ca
SangViet <99493464+SangViet@users.noreply.github.com>
2022-02-14 22:22:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     titleWeb: "Gửi Cho Em Ngày Đặc Biệt :>3 ",
     introTitle: 'Trần Thị Ánh',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    introDesc: `Nếu anh không gặp em, anh sẽ không thích em. Nếu anh không thích em, anh sẽ không yêu em. 
+    Nếu anh không yêu em, anh sẽ không nhớ em. Nhưng anh đã làm, đang làm và sẽ làm. Yêu em thật nhiều… Yêu em!
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnIntro: '^^Yêu^^',
+    title: 'Lần đầu tiên em nhìn thấy anh, em đã thấy tình yêu 🥰',
+    desc: 'Lần đầu tiên anh nhìn thấy em, anh đã thấy tình yêu ',
     btnYes: 'Vẫn Cứ thích Anh <33',
     btnNo: 'Không anh trai àk :3',
     question: 'Tại sao em lại yêu anh <3',
     btnReply: 'Gửi cho Anh <3',
-    reply: 'Yêu thì yêu mà không yêu cũng phải yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu  nhiều nhiều 😘😘',
+    reply: 'em viết cho anh: { EM YÊU ANH } <33333333',
+    mess: 'Anh biết mà 🥰. Yêu   nhiều 😘😘',
     messDesc: 'làm người anh yêu nhé :>3',
     btnAccept: 'Okiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
