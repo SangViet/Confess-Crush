5e03fe56fe9f6ad9740930f1a9cb0504d0c4c849
SangViet <99493464+SangViet@users.noreply.github.com>
2022-02-14 22:33:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    titleWeb: "Ngày Lễ Valentine ",
+    introTitle: 'Trần Thị Ánh',
+    introDesc: `Nếu anh không gặp em, anh sẽ không thích em. Nếu anh không thích em, anh sẽ không yêu em. 
+    Nếu anh không yêu em, anh sẽ không nhớ em. Nhưng anh đã làm, đang làm và sẽ làm. Yêu em thật nhiều… 
+    Yêu em!`,
+    btnIntro: '^^Yêu^^',
+    title: 'Lần đầu tiên em nhìn thấy anh, em đã thấy tình yêu 🥰',
+    desc: 'Lần đầu tiên anh nhìn thấy em, anh đã thấy tình yêu ',
+    btnYes: 'Thích Lắm Anh Trai <33',
+    btnNo: 'Không Anh Trai À! :3',
+    question: 'Tại sao em lại yêu anh <3',
+    btnReply: 'Gửi cho Anh <3',
+    reply: 'Em viết cho anh: { EM YÊU ANH }  <33333333',
+    mess: 'Anh biết mà 🥰. Yêu Em nhiều nhiều 😘😘',
+    messDesc: 'làm người anh yêu nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
